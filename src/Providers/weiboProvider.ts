156db292cd229b4871220caa31f14dd255fd61e1
yangliwei <1280426581@qq.com>
2024-11-26 10:55:14 +0800

    feat: 登录失效情况判断

--- src/Providers/weiboProvider.ts
+++ src/Providers/weiboProvider.ts
@@ -1,15 +1,16 @@
 /*
  * @Author: yangliwei 1280426581@qq.com
  * @Date: 2024-11-12 15:14:35
- * @LastEditTime: 2024-11-22 16:18:13
+ * @LastEditTime: 2024-11-26 10:54:33
  * @LastEditors: yangliwei 1280426581@qq.com
  * @FilePath: \touchfish\src\Providers\weiboProvider.ts
  * Copyright (c) 2024 by yangliwei, All Rights Reserved. 
  * @Description: 
  */
-import { WebviewView, WebviewViewProvider, ExtensionContext, Uri } from 'vscode';
+import { WebviewView, WebviewViewProvider, ExtensionContext, Uri, window } from 'vscode';
 import { getWeiboComment, getWeiboData, getWeiboImg } from '../api/weibo';
 import { commandsType, weiboAJAX } from '../../type';
+import { setConfigByKey } from '../config';
 export class WeiboProvider implements WebviewViewProvider {
   constructor(protected context: ExtensionContext) { }
 
@@ -35,7 +36,22 @@ export class WeiboProvider implements WebviewViewProvider {
       switch (message.command) {
         case "GETDATA":
           {
-            const res = await getWeiboData(message.payload);
+            let res = await getWeiboData(message.payload);
+            console.log("GETDATA", res.data);
+            if (res.data.ok !== 1) {
+              if (res.data.ok == -100) {
+                const cookie = await window.showInputBox({
+                  placeHolder: "请输入微博的cookie",
+                  prompt: "请输入微博的cookie",
+                });
+                if (cookie) {
+                  await setConfigByKey('weiboCookie', cookie);
+                  res = await getWeiboData(message.payload);
+                }
+              } else {
+                window.showInformationMessage("获取微博数据失败!");
+              }
+            }
             webviewView.webview.postMessage({
               command: "SENDDATA",
               payload: res.data
@@ -57,12 +73,12 @@ export class WeiboProvider implements WebviewViewProvider {
             webviewView.webview.postMessage({
               command: `SENDCOMMENT`,
               payload: {
-                payload:message.payload,
+                payload: message.payload,
                 ...res.data
               }
             } as commandsType<weiboAJAX>)
             break;
-        }
+          }
       }
     })
 
