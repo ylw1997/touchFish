b9c4a03c6888aa1c740f3a4e3b13456ceaadae6e
YangLiwei <1280426581@qq.com>
2025-06-19 15:40:01 +0800

    feat: vscode支持开发环境开发

--- src/Providers/weiboProvider.ts
+++ src/Providers/weiboProvider.ts
@@ -1,41 +1,51 @@
 /*
  * @Author: yangliwei 1280426581@qq.com
  * @Date: 2024-11-12 15:14:35
- * @LastEditTime: 2025-06-18 16:43:38
+ * @LastEditTime: 2025-06-19 15:00:31
  * @LastEditors: YangLiwei 1280426581@qq.com
  * @FilePath: \touchfish\src\Providers\weiboProvider.ts
- * Copyright (c) 2024 by yangliwei, All Rights Reserved. 
- * @Description: 
+ * Copyright (c) 2024 by yangliwei, All Rights Reserved.
+ * @Description:
  */
-import { WebviewView, WebviewViewProvider, ExtensionContext, Uri, window } from 'vscode';
-import { followUser, getLongText, getUserWeibo, getWeiboComment, getWeiboData, getWeiboImg, sendWeibo } from '../api/weibo';
-import { commandsType, weiboAJAX } from '../../type';
-import { setConfigByKey } from '../config';
+import {
+  WebviewView,
+  WebviewViewProvider,
+  ExtensionContext,
+  Uri,
+  window,
+} from "vscode";
+import {
+  followUser,
+  getLongText,
+  getUserWeibo,
+  getWeiboComment,
+  getWeiboData,
+  getWeiboImg,
+  sendWeibo,
+} from "../api/weibo";
+import { commandsType, weiboAJAX } from "../../type";
+import { setConfigByKey } from "../config";
 export class WeiboProvider implements WebviewViewProvider {
-  constructor(protected context: ExtensionContext) { }
+  constructor(protected context: ExtensionContext) {}
 
   public resolveWebviewView(webviewView: WebviewView): Thenable<void> | void {
     webviewView.webview.options = {
       enableScripts: true,
-      localResourceRoots: [
-        this.context.extensionUri
-      ]
-    }
+      localResourceRoots: [this.context.extensionUri],
+    };
 
-    const scriptUri = webviewView.webview.asWebviewUri(Uri.joinPath(
-      this.context.extensionUri,
-      'weibo/dist/index.js'
-    ))
+    const scriptUri = webviewView.webview.asWebviewUri(
+      Uri.joinPath(this.context.extensionUri, "weibo/dist/index.js")
+    );
 
-    const cssUri = webviewView.webview.asWebviewUri(Uri.joinPath(
-      this.context.extensionUri,
-      'weibo/dist/index.css'
-    ))
+    const cssUri = webviewView.webview.asWebviewUri(
+      Uri.joinPath(this.context.extensionUri, "weibo/dist/index.css")
+    );
 
-    webviewView.webview.onDidReceiveMessage(async (message: commandsType<string>) => {
-      switch (message.command) {
-        case "GETDATA":
-          {
+    webviewView.webview.onDidReceiveMessage(
+      async (message: commandsType<string>) => {
+        switch (message.command) {
+          case "GETDATA": {
             let res = await getWeiboData(message.payload);
             // console.log("GETDATA", res.data);
             if (res.data.ok !== 1) {
@@ -45,7 +55,7 @@ export class WeiboProvider implements WebviewViewProvider {
                   prompt: "请输入微博的cookie",
                 });
                 if (cookie) {
-                  await setConfigByKey('weiboCookie', cookie);
+                  await setConfigByKey("weiboCookie", cookie);
                   res = await getWeiboData(message.payload);
                 }
               } else {
@@ -54,84 +64,115 @@ export class WeiboProvider implements WebviewViewProvider {
             }
             webviewView.webview.postMessage({
               command: "SENDDATA",
-              payload: res.data
-            } as commandsType<weiboAJAX>)
+              payload: res.data,
+            } as commandsType<weiboAJAX>);
             break;
           }
-        case "GETIMG":
-          {
+          case "GETIMG": {
             const res = await getWeiboImg(message.payload);
             webviewView.webview.postMessage({
               command: `SENDIMG:${message.payload}` as any,
-              payload: res
-            } as commandsType<string>)
+              payload: res,
+            } as commandsType<string>);
             break;
           }
-        case "GETCOMMENT":
-          {
+          case "GETCOMMENT": {
             const res = await getWeiboComment((message.payload as any).url);
             webviewView.webview.postMessage({
               command: `SENDCOMMENT`,
               payload: {
                 payload: message.payload,
-                ...res.data
-              }
-            } as commandsType<weiboAJAX>)
+                ...res.data,
+              },
+            } as commandsType<weiboAJAX>);
             break;
           }
-        case "GETLONGTEXT":
-          {
+          case "GETLONGTEXT": {
             const res = await getLongText(message.payload);
             webviewView.webview.postMessage({
               command: `SENDLONGTEXT`,
               payload: {
                 payload: message.payload,
-                ...res.data
-              }
-            } as commandsType<weiboAJAX>)
+                ...res.data,
+              },
+            } as commandsType<weiboAJAX>);
             break;
           }
-        case "GETUSERBLOG":
-          {
+          case "GETUSERBLOG": {
             const res = await getUserWeibo(message.payload);
             webviewView.webview.postMessage({
               command: `SENDUSERBLOG`,
               payload: {
                 payload: message.payload,
-                ...res.data
-              }
-            } as commandsType<weiboAJAX>)
+                ...res.data,
+              },
+            } as commandsType<weiboAJAX>);
             break;
           }
-        case "GETFOLLOW":
-          {
+          case "GETFOLLOW": {
             const res = await followUser(message.payload);
             webviewView.webview.postMessage({
               command: `SENDFOLLOW`,
               payload: {
                 payload: message.payload,
-                ...res.data
-              }
-            } as commandsType<weiboAJAX>)
+                ...res.data,
+              },
+            } as commandsType<weiboAJAX>);
             break;
           }
-        case "GETNEWBLOGRESULT":{
-          const res = await sendWeibo(message.payload);
-          webviewView.webview.postMessage({
-              command: 'SENTNEWBLOGRESULT',
+          case "GETNEWBLOGRESULT": {
+            const res = await sendWeibo(message.payload);
+            webviewView.webview.postMessage({
+              command: "SENTNEWBLOGRESULT",
               payload: {
                 payload: message.payload,
-                ...res.data
-              }
-            } as commandsType<weiboAJAX>)
-          break;
+                ...res.data,
+              },
+            } as commandsType<weiboAJAX>);
+            break;
+          }
         }
       }
-    })
+    );
 
-    webviewView.webview.html = `
-      <!DOCTYPE html>
-        <html lang="en"></html>
+    // 判断是否为开发环境
+    const isDev =
+      process.env.NODE_ENV === "development" ||
+      process.env.VSCODE_DEBUG_MODE === "true";
+    let htmlContent = "";
+    console.log("isDev", isDev);
+    if (isDev) {
+      // 开发环境，直接加载本地Vite dev server
+      htmlContent = `
+        <!doctype html>
+                <html lang="en">
+          <head>
+            <script type="module">
+        import RefreshRuntime from "http://localhost:5173/@react-refresh"
+        RefreshRuntime.injectIntoGlobalHook(window)
+        window.$RefreshReg$ = () => {}
+        window.$RefreshSig$ = () => (type) => type
+        window.__vite_plugin_react_preamble_installed__ = true
+        </script>
+
+            <script type="module" src="http://localhost:5173/@vite/client"></script>
+
+            <meta charset="UTF-8" />
+            <link rel="icon" type="image/svg+xml" href="http://localhost:5173/vite.svg" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Vite + React + TS</title>
+          </head>
+          <body>
+            <div id="root"></div>
+            <script type="module" src="http://localhost:5173/src/main.tsx"></script>
+          </body>
+        </html>
+      `;
+    } else {
+      // 生产环境，加载打包后的静态资源
+      htmlContent = `
+        <!DOCTYPE html>
+        <html lang="en">
           <head>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -144,6 +185,7 @@ export class WeiboProvider implements WebviewViewProvider {
           </body>
         </html>
       `;
+    }
+    webviewView.webview.html = htmlContent;
   }
-
 }
