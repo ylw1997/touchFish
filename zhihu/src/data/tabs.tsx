7a50323f1273edb37e224a4539491da259d4b6de
YangLiwei <1280426581@qq.com>
2025-08-14 18:08:41 +0800

    refactor(zhihu): 简化人气问题标签文本
    - 将知乎热榜中的人气问题标签文本从"人气问题"简化为"人气"
    - 更新 package.json 版本号至 8.5.1

--- zhihu/src/data/tabs.tsx
+++ zhihu/src/data/tabs.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: YangLiwei 1280426581@qq.com
  * @Date: 2025-08-07 09:19:24
- * @LastEditTime: 2025-08-14 17:52:47
+ * @LastEditTime: 2025-08-14 18:06:53
  * @LastEditors: YangLiwei 1280426581@qq.com
  * @FilePath: \touchfish\zhihu\src\data\tabs.tsx
  * Copyright (c) 2025 by YangLiwei, All Rights Reserved.
@@ -15,6 +15,6 @@ export const defTab = [
   { key: "hot", label: "热榜" },
   {
     key: "hot_question",
-    label: "人气问题",
+    label: "人气",
   },
 ] as { key: tabKeyType; label: string }[];
