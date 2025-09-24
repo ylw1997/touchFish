e55a579644894413fae91821b61b2f9e73cd299d
YangLiwei <1280426581@qq.com>
2025-09-24 14:45:18 +0800

    fix(loader): 更新文件编辑时间并调整样式
    更新了 loader.tsx 文件的最后编辑时间，并在样式中添加

--- weibo/src/utils/loader.tsx
+++ weibo/src/utils/loader.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: YangLiwei 1280426581@qq.com
  * @Date: 2025-06-18 14:25:26
- * @LastEditTime: 2025-09-12 16:19:49
+ * @LastEditTime: 2025-09-24 14:44:58
  * @LastEditors: YangLiwei 1280426581@qq.com
  * @FilePath: \touchfish\weibo\src\utils\loader.tsx
  * Copyright (c) 2025 by YangLiwei, All Rights Reserved.
@@ -15,6 +15,7 @@ export const loaderFunc = () => {
       styles={{
         body: {
           padding: "8px",
+          marginBottom: "8px",
         },
       }}
     >
