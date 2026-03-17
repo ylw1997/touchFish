644b0b9d90e5c1a3ecbcb220c204da56be33c775
yangliwei <1280426581@qq.com>
2026-03-17 14:53:21 +0800

    feat: 优化打包文件

--- regex_test.js
+++
@@ -1,3 +0,0 @@
-const html = `var __PAGE = {0:'/read.php?tid=46171081',1:5,2:5,3:20};commonui.pageBtn(document.getElementById('pageBtnHere').parentNode,__PAGE,true)`;
-const match = html.match(/__PAGE\s*=\s*\{.*?\b1\s*:\s*(\d+)/);
-console.log(match ? match[1] : "No match");
