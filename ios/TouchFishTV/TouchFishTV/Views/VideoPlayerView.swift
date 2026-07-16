256a8487cb5b5323e46671f908ad57d20d2a9366
yangliwei <1280426581@qq.com>
2026-07-16 14:35:51 +0800

    fix(tvos): 进一步缩小原生播放标题

--- ios/TouchFishTV/TouchFishTV/Views/VideoPlayerView.swift
+++ ios/TouchFishTV/TouchFishTV/Views/VideoPlayerView.swift
@@ -111,7 +111,9 @@ final class DouyinPlayerViewController: AVPlayerViewController {
         transportBarIncludesTitleView = true
         // AVPlayerViewController 的标题字体不能单独设置；使用公开的动态字号
         // trait 缩小原生信息区，同时继续保留系统进度条与控制栏。
-        traitOverrides.preferredContentSizeCategory = .medium
+        traitOverrides.preferredContentSizeCategory = .extraSmall
+        view.traitOverrides.preferredContentSizeCategory = .extraSmall
+        updateTraitsIfNeeded()
         focusAnchor.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(focusAnchor)
         NSLayoutConstraint.activate([
